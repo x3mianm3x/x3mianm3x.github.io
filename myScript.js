05607e0cd5bba9acacf51d1879c9430bea918838
x3mianm3x <christianroque0829@gmail.com>
2016-09-28 22:25:04 +0800

    Add files via upload
    Sample Template for a Real Estate Project

---
+++ myScript.js
@@ -0,0 +1,29 @@
+
+var banner = document.getElementById('banner');
+var paragraph = document.getElementById('paragraphChange');
+function changeText1(){
+	document.getElementById("headerChange").innerHTML = "Lorem Ipsum";
+	document.getElementById("paragraphChange").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sem quam, vehicula vitae sagittis eget, ornare id mi. Ut eleifend facilisis quam, in luctus erat laoreet in. Sed nec magna diam, et egestas augue. In fermentum sagittis tellus, interdum scelerisque turpis commodo quis. Curabitur iaculis tempor mollis. Donec sed neque quis ligula ornare iaculis et eget urna. Nullam nec venenatis nisi. Sed tincidunt, elit facilisis lobortis congue, eros magna porttitor massa, non fermentum quam dolor quis risus. Nullam tortor augue, pretium eget scelerisque in, feugiat id metus. Vivamus nec felis lectus. Morbi volutpat vestibulum ligula, ut sodales urna suscipit non. Phasellus eu mauris massa, sit amet fermentum turpis. Vestibulum faucibus tincidunt orci vel  tempor. Nullam dolor est, adipiscing et sagittis sed, tristique vitae tortor.";
+    banner.style.background = 'rgba(75,73,71, 0.9)';
+    paragraph.style.color ='#EAEAEA';
+}
+
+function changeText2(){
+	document.getElementById("headerChange").innerHTML ="Lorem Lorem ";
+	document.getElementById("paragraphChange").innerHTML ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sem quam, vehicula vitae sagittis eget, ornare id mi. Ut eleifend facilisis quam, in luctus erat laoreet in. Sed nec magna diam, et egestas augue. In fermentum sagittis tellus, interdum scelerisque turpis commodo quis. Curabitur iaculis tempor mollis. Donec sed neque quis ligula ornare iaculis et eget urna. Nullam nec venenatis nisi. Sed tincidunt, elit facilisis lobortis congue, eros magna porttitor massa, non fermentum quam dolor quis risus. Nullam tortor augue, pretium eget scelerisque in, feugiat id metus. Vivamus nec felis lectus. Morbi volutpat vestibulum ligula, ut sodales urna suscipit non. Phasellus eu mauris massa, sit amet fermentum turpis. Vestibulum faucibus tincidunt orci vel  tempor. Nullam dolor est, adipiscing et sagittis sed, tristique vitae tortor.";
+    banner.style.background = 'rgba(244,44,44, 0.9)';
+    paragraph.style.color ='#444444';
+
+}
+function changeText3(){
+	document.getElementById("headerChange").innerHTML ="Ipsum Ipsum";
+	document.getElementById("paragraphChange").innerHTML ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sem quam, vehicula vitae sagittis eget, ornare id mi. Ut eleifend facilisis quam, in luctus erat laoreet in. Sed nec magna diam, et egestas augue. In fermentum sagittis tellus, interdum scelerisque turpis commodo quis. Curabitur iaculis tempor mollis. Donec sed neque quis ligula ornare iaculis et eget urna. Nullam nec venenatis nisi. Sed tincidunt, elit facilisis lobortis congue, eros magna porttitor massa, non fermentum quam dolor quis risus. Nullam tortor augue, pretium eget scelerisque in, feugiat id metus. Vivamus nec felis lectus. Morbi volutpat vestibulum ligula, ut sodales urna suscipit non. Phasellus eu mauris massa, sit amet fermentum turpis. Vestibulum faucibus tincidunt orci vel  tempor. Nullam dolor est, adipiscing et sagittis sed, tristique vitae tortor.";
+    banner.style.background = 'rgba(103,160,250, 0.9)';
+    paragraph.style.color ='#444444';
+}
+function changeText4(){
+	document.getElementById("headerChange").innerHTML ="Magna Ipsum";
+	document.getElementById("paragraphChange").innerHTML ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sem quam, vehicula vitae sagittis eget, ornare id mi. Ut eleifend facilisis quam, in luctus erat laoreet in. Sed nec magna diam, et egestas augue. In fermentum sagittis tellus, interdum scelerisque turpis commodo quis. Curabitur iaculis tempor mollis. Donec sed neque quis ligula ornare iaculis et eget urna. Nullam nec venenatis nisi. Sed tincidunt, elit facilisis lobortis congue, eros magna porttitor massa, non fermentum quam dolor quis risus. Nullam tortor augue, pretium eget scelerisque in, feugiat id metus. Vivamus nec felis lectus. Morbi volutpat vestibulum ligula, ut sodales urna suscipit non. Phasellus eu mauris massa, sit amet fermentum turpis. Vestibulum faucibus tincidunt orci vel  tempor. Nullam dolor est, adipiscing et sagittis sed, tristique vitae tortor.";
+    banner.style.background = 'rgba(255,126,21, 0.9)';
+    paragraph.style.color ='#444444';
+}
